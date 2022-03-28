dadc7628859886f65d34f3ec7efd01bb16556a30
pedro-vini <pedro-vini@test.com>
2022-03-28 10:22:18 -0300

    Desenvolvimento do exercicio 7.

---
+++ src/ExercicioSete.java
@@ -0,0 +1,20 @@
+
+public class ExercicioSete {
+	public static void main(String[] args) {
+		
+		double ladosiguais = 2;
+		
+		if (ladosiguais == 3)
+		{
+			System.out.println("Triângulo Equilátero");
+	    }
+	    else if (ladosiguais == 2)
+		{
+		System.out.println("Triângulo Isósceles");	
+		}
+	    else {
+	    	System.out.println("Triângulo Escaleno");
+	    }
+	    	
+	}
+}

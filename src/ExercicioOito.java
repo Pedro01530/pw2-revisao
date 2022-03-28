94cb328a09adcd00f299c4f5b352a76ac1cb97d3
pedro-vini <pedro-vini@test.com>
2022-03-28 10:36:59 -0300

    Desenvolvimento do exercicio 8

---
+++ src/ExercicioOito.java
@@ -0,0 +1,25 @@
+
+public class ExercicioOito {
+public static void main(String[] args) {
+	
+	String diaSemana = "domingo";
+	String segunda;
+	String quarta;
+	String quinta;
+	String sexta;
+	String sabado;
+	String domingo;
+	
+	if (diaSemana == "domingo") {
+		System.out.println("O banco esta fechado tente outro dia");
+	}
+	else if(diaSemana == "sabado") {
+		System.out.println("O banco esta fechado tente outro dia");
+	}
+	else
+	{
+		System.out.println("O banco esta aberto");
+	}
+	
+}
+}
